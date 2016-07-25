4c07e5d88b4853ec0981bda44f1cba8875e9d7c3
jh <jh@2mbgame.com>
2016-07-25 13:33:52 +0900

    - 유니티 5.0의 에셋 번들에 대해 공부해보자 part 2. 생성과 빌드편

---
+++ Assets/Editor/BuildAssetBundles.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * http://docs.unity3d.com/kr/current/Manual/BuildingAssetBundles.html
+ */
+using UnityEditor;
+
+public class BuildAssetBundles
+{
+    // MenuItem을 사용하면 위의 메뉴창에 새로운 메뉴를 추가할 수 있습니다.
+    // 아래의 코드에서는 Bundles 항목에 하위항목으로 Build AssetBundle항목을 추가하도록 합니다.
+    [MenuItem ("Bundles/Build AssetBundles")]
+    static void BuildAllAssetBundles ()
+    {
+        // BuildPipeLine 클래스의 클래스 함수인 BuildAssetBundles 함수입니다. 함수 그대로 에셋 번들을 만들어줍니다.
+        // BuildAssetBunldes() 함수 안은 String 매개변수가 필요합니다. 이 매개변수는 빌드된 에셋 번들을 저장할 경로입니다.
+        // 예를 들어 Assets폴더의 하위 폴더 AssetBundles 폴더에 저장하려면 "Assets/AssetBundles"라고 입력해야겠지요.
+        BuildPipeline.BuildAssetBundles("AssetBundles");
+    }
+}
---
+++ Assets/Scripts/AutoMove.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Direction
+{
+    Left = 0,
+    Top,
+    Right,
+    Bottom,
+    Front,
+    Back       
+};
+
+public class AutoMove : MonoBehaviour 
+{
+    public float moveSpeed = 2.0f;
+    public Direction direction = Direction.Top;
+
+    void Update()
+    {
+        Vector3 v = transform.position;
+        switch (direction)
+        {
+            case Direction.Left:
+                v.x += moveSpeed * Time.deltaTime;
+                break;
+            case Direction.Top:
+                v.y += moveSpeed * Time.deltaTime;
+                break;
+            case Direction.Right:
+                v.x -= moveSpeed * Time.deltaTime;
+                break;
+            case Direction.Bottom:
+                v.y -= moveSpeed * Time.deltaTime;
+                break;
+            case Direction.Front:
+                v.z += moveSpeed * Time.deltaTime;
+                break;
+            case Direction.Back:
+                v.z -= moveSpeed * Time.deltaTime;
+                break;
+        }
+        transform.position = v; 
+    }
+
+}
