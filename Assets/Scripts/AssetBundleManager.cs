40c5e5d039ad3982646ca8ca61a47062b1e6330a
jh <jh@2mbgame.com>
2016-07-25 15:48:31 +0900

    - 에셋 번들 매니저에 도전해보자! part2. 코드편

---
+++ Assets/Scripts/AssetBundleManager.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AssetBundleManager : MonoBehaviour 
+{
+    private static AssetBundleManager _instance = null;
+
+    // 매니저를 만듬에 있어서 Class 자체를 static으로 지정하여 사용할 수도 있습니다.
+    // 저의 경우엔 static으로 자신을 하나 물고 있고, AssetBundleManager.Instance.Anythine()의 형식으로 접근하는 방식이 편해서
+    // 이렇게 작성을 하였습니다. 클래스를 static 선언하시고 생성자에서 초기화해도 상관 없습니다.
+    public static AssetBundleManager Instance
+    {
+        get
+        {
+            // 처음 호출시엔 인스턴스가 물려있을리 없으니 게임오브젝트에 에셋번들매니저 클래스를 붙이고 물려줍니다.
+            if(_instance == null)
+            {
+                GameObject go = new GameObject("_AssetBundleManager");
+                go.isStatic = true;
+                _instance = go.AddComponent<AssetBundleManager>();
+
+                dicAssetBundle = new Dictionary<string, AssetBundleNode>();
+                lstKeyName = new List<string>();
+            }
+            return _instance;             
+        }
+    }
+    
+    // 에셋 번들을 저장할 노드입니다. 에셋번들, url, 버전, unload시 bool 조건값을 저장합니다.
+    public class AssetBundleNode
+    {
+        public AssetBundle assetBundle;
+        public string url;
+        public int version;
+        public bool removeAll;
+
+        public AssetBundleNode(string url, int version, bool removeAll, AssetBundle assetBundle)
+        {
+            this.url = url;
+            this.version = version;
+            this.removeAll = removeAll;
+            this.assetBundle = assetBundle;
+        }
+
+        // 물려있는 에셋번들의 unload() 함수를 실행해줍니다.
+        public void UnloadAssetBundle()
+        {
+            assetBundle.Unload(removeAll);
+        }
+    }
+
+    private static Dictionary<string, AssetBundleNode> dicAssetBundle;
+    // save keyName for remove All ABs
+    private static List<string> lstKeyName;
+
+    // 에셋 번들을 로드하여 Dic에 저장
+    public IEnumerator LoadAssetBundle(string url, int version, bool removeAll)
+    {
+        string keyName = MakeKeyName(url, version);
+        // 이 부분 주석처리한 이유는 역시나 캐싱은 다운로드시에나 필요한 것이기 때문에..
+        // 게임중에 캐싱해야될 일이 생긴다는 건 크래시나 프레임드랍 등에 노출된다는 상황입니다.
+        // 캐싱가능할때까지 대기
+        /*
+        while(!Caching.ready)
+        {
+            yield return null;
+        }*/
+
+        // 중복 로드되는 상황을 막습니다.
+        if(IsVersionAdded(url, version))
+        {
+            //yield return null;
+        }
+        else
+        {
+            // 로드 되지 않은 경우, 노드를 만들고 Dic에 저장.
+            using(WWW www = WWW.LoadFromCacheOrDownload(url, version))
+            {
+                yield return www;
+                if(www.error == null)
+                {
+                    AssetBundleNode node = new AssetBundleNode(url, version, removeAll, www.assetBundle);
+                    dicAssetBundle.Add(keyName, node);
+                    lstKeyName.Add(keyName);
+                }
+            }
+        }
+    }
+
+    // 위의 LoadAssetBundle() 함수를 통하여 불러온 에셋 번들을 리턴 시키는 함수.
+    public AssetBundle GetAssetBundle(string url, int version)
+    {
+        string keyName = MakeKeyName(url, version);
+        return dicAssetBundle[keyName].assetBundle;
+    }
+
+    // 받아온 url, version 정보로 저장된 에셋번들이 있는지 확인
+    public bool IsVersionAdded(string url, int version)
+    {
+        string keyName = MakeKeyName(url, version);
+        return dicAssetBundle.ContainsKey(keyName);       
+    }
+
+    // url, version 정보를 받아와 keyName을 만듭니다.
+    public string MakeKeyName(string url, int version)
+    {
+        return url + version;
+    }
+
+    // 에셋 번들을 unload 시키는 함수입니다. 반드시 Dic에서 Remove 시키기 전에 에셋 번들을 unload 시킵시다.
+    public void RemoveAssetBundle(string url, int version)
+    {
+        string keyName = MakeKeyName(url, version);
+        RemoveAssetBundle(keyName);
+    }
+
+    // 위의 함수와 같은 함수입니다. 
+    public void RemoveAssetBundle(string keyName)
+    {
+        if (dicAssetBundle.ContainsKey(keyName))
+        {
+            dicAssetBundle[keyName].UnloadAssetBundle();
+            dicAssetBundle.Remove(keyName);
+            lstKeyName.Remove(keyName);
+        }
+    }
+
+    // 현재 저장되어 있는 에셋번들의 갯수를 리턴
+    public int GetNumberOfABs()
+    {
+        return dicAssetBundle.Count;
+    }
+
+    // 저장되어 있는 모든 에셋 번들을 Unload 시킵니다.
+    public void RemoveAllAssetBundles()
+    {
+        for(int i = 0; i < dicAssetBundle.Count; i++)
+        {
+            dicAssetBundle[lstKeyName[i]].UnloadAssetBundle();
+        }
+        dicAssetBundle.Clear();
+        lstKeyName.Clear();
+    }
+}
---
+++ Assets/Scripts/UseEx.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UseEx : MonoBehaviour 
+{
+    public string url = "http://localhost/AssetBundles/assetbundle_0";
+    public int version = 0;
+
+    IEnumerator Start()
+    {
+        // LoadAssetBundle(url, version, unload매개변수)를 실행하고 이 함수의 실행이 끝날 때까지 기다립니다.
+        yield return StartCoroutine(AssetBundleManager.Instance.LoadAssetBundle(url, version, false));
+        
+        // 에셋번들이 불러와 졌는지 확인.
+        Debug.Log(AssetBundleManager.Instance.IsVersionAdded(url, version));
+        
+        // 에셋번들이 몇개 저장되어 있는지 확인.
+        Debug.Log(AssetBundleManager.Instance.GetNumberOfABs());
+
+        // GetAssetBundle(url, version) 함수를 통해 에셋번들을 리턴받아 참조
+        AssetBundle ab = AssetBundleManager.Instance.GetAssetBundle(url, version);
+
+        // 참조한 에셋 번들에서 Cube 1 에셋을 불러왔습니다.
+        GameObject gObj = ab.LoadAsset("Cube 1") as GameObject;
+        Instantiate(gObj, Vector3.zero, Quaternion.identity);
+
+        // removeAllAssetBundles() 함수를 실행하여 Dic을 비웁니다.
+        AssetBundleManager.Instance.RemoveAllAssetBundles();
+
+        // unload 되었는지 확인. 0이 나와야 정상.
+        Debug.Log( AssetBundleManager.Instance.GetNumberOfABs() );
+
+        // 컴퓨터의 임의의 Cache 폴더에 용량이 쌓이는 것을 방지하기 위해 테스트시에는 Caching.CleanCache()를 써줍니다.
+        Caching.CleanCache();
+    }
+}
